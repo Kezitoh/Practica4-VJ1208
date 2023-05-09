c1ca263e390e6e33741cac86f0e6da6ee65d72ff
ngadirov <nezirgadirov@gmail.com>
2023-05-09 18:51:37 +0200

    ???

--- Prac4Ej1/Prac4Ej1/ListaNodos.cs
+++ Prac4Ej1/Prac4Ej1/ListaNodos.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Prac4Ej1
 {
     internal class ListaNodos
@@ -25,7 +26,14 @@ namespace Prac4Ej1
 
         public void Add(Object valor)
         {
-
+            Nodo nuevo = new Nodo(valor);
+            if (Count == 0) primero = ultimo = nuevo;
+            else
+            {
+                ultimo.Sig = nuevo;
+                ultimo = nuevo;
+            }
+            Count++;
         }
 
         public bool Contains(Object valor)
@@ -35,7 +43,19 @@ namespace Prac4Ej1
 
         public void Remove(Object valor)
         {
-
+            Nodo iterador = primero.Sig;
+            Nodo anterior = primero;
+            while(iterador != null)
+            {
+                if (iterador.Dato.Equals(valor))
+                {
+                    anterior.Sig = iterador.Sig;
+                    Count--;
+                    return;
+                }
+                anterior = iterador;
+                iterador = iterador.Sig;
+            }
         }
 
         public Object ElementAt(int indice)
--- Prac4Ej1/Prac4Ej1/Program.cs
+++ Prac4Ej1/Prac4Ej1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ListaNodos.cs;
 
 namespace Prac4Ej1
 {
@@ -11,6 +12,9 @@ namespace Prac4Ej1
 
         static void Main(string[] args)
         {
+            ListaNodos listaNodos = new ListaNodos();
+
+            listaNodos.Add(new Nodo(12));
         }
     }
 }
