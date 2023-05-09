2a8bd11bfd380752ab2b1cd776fad15630151f7c
Alex Deb Duq <alejandrodebduq@gmail.com>
2023-05-09 18:28:32 +0200

    Estado inicial de la práctica

---
+++ Prac4Ej1/Prac4Ej1/ListaNodos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac4Ej1
+{
+    internal class ListaNodos
+    {
+        private Nodo primero;
+        private Nodo ultimo;
+        public int Count { get; private set; }
+
+        public ListaNodos()
+        {
+            primero = ultimo = null;
+            Count = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return (Count == 0);
+        }
+
+        public void Add(Object valor)
+        {
+
+        }
+
+        public bool Contains(Object valor)
+        {
+            return false;
+        }
+
+        public void Remove(Object valor)
+        {
+
+        }
+
+        public Object ElementAt(int indice)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Nada";
+        }
+    }
+}
---
+++ Prac4Ej1/Prac4Ej1/Nodo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac4Ej1
+{
+    internal class Nodo
+    {
+        public Object Dato { get; set; }
+        public Nodo Sig { get; set; }
+        public Nodo(object dato, Nodo sig)
+        {
+            this.Dato = dato;
+            this.Sig = sig;
+        }
+    }
+}
